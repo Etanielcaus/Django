ccfd27aabc2cafb98e464dd1edbb05bf15bf3b07
Etanielcaus <nielfilosofia@gmail.com>
2023-09-30 10:19:20 -0300

    django

--- blog/views.py
+++ blog/views.py
@@ -4,14 +4,24 @@ from django.shortcuts import render
 
 
 def blog(request):
+    context = {
+        'text': 'Página do Blog',
+        'title': 'Estamos no Blog - '
+    }
     return render(
         request,
-        'blog/index.html'
+        'blog/index.html',
+        context
     )
 
 
 def exemplo(request):
+    context = {
+        'text': 'Estamos no Exemplo',
+        'title': 'Essa é uma pagina de exemplo - '
+    }
     return render(
         request,
-        'blog/exemplo.html'
+        'blog/exemplo.html',
+        context
     )
